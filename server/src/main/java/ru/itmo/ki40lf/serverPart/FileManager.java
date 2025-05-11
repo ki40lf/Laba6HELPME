097d9b64cc71e06b201b96a6bbd804ecbcf60f46
ki40lf <shirokovlev2006@gmail.com>
2025-05-11 19:08:25 +0300

    git commit

--- server/src/main/java/ru/itmo/ki40lf/serverPart/FileManager.java
+++ server/src/main/java/ru/itmo/ki40lf/serverPart/FileManager.java
@@ -1,12 +1,72 @@
 package ru.itmo.ki40lf.serverPart;
 
-import ru.itmo.ki40lf.resources.Dragon;
+import ru.itmo.ki40lf.resources.*;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class FileManager {
+    private final String filePath;
 
+    public FileManager(String filePath) {
+        this.filePath = filePath;
+    }
+
+    // Чтение из CSV файла
+    public List<Dragon> readFromCSV() {
+        List<Dragon> dragons = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                // Предполагается, что порядок колонок соответствует полям Dragon
+                Dragon dragon = new Dragon(
+                        Integer.parseInt(nextLine[0]),
+                        nextLine[1],
+                        new Coordinates(Float.parseFloat(nextLine[2]), Double.parseDouble(nextLine[3])),
+                        ZonedDateTime.parse(nextLine[4], DateTimeFormatter.ISO_ZONED_DATE_TIME),
+                        Long.parseLong(nextLine[5]),
+                        Color.valueOf(nextLine[6]),
+                        DragonType.valueOf(nextLine[7]),
+                        DragonCharacter.valueOf(nextLine[10]),
+                        new DragonCave(
+                                Double.parseDouble(nextLine[8]),    // Depth
+                                nextLine[9].equals("null") ? null : Float.parseFloat(nextLine[9])
+                        )
+
+                );
+                dragons.add(dragon);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dragons;
+    }
+
+    // Запись в CSV файл
+    public void writeToCSV(List<Dragon> dragons) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
+            for (Dragon dragon : dragons) {
+                String[] record = {
+                        String.valueOf(dragon.getId()),
+                        dragon.getName(),
+                        String.valueOf(dragon.getCoordinates().getX()),
+                        String.valueOf(dragon.getCoordinates().getY()),
+                        dragon.getCreationDate().format(DateTimeFormatter.ISO_ZONED_DATE_TIME),
+                        String.valueOf(dragon.getAge()),
+                        dragon.getColor().name(),
+                        dragon.getType().name(),
+                        String.valueOf(dragon.getCave().getDepth()),
+                        dragon.getCave().getNumberOfTreasures() == null ? "null" : String.valueOf(dragon.getCave().getNumberOfTreasures())
+                };
+                writer.writeNext(record);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
