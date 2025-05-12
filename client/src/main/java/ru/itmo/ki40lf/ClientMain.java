1cad584061b76ba14626454f0422254af6758fe3
Yura <lubsterplay0703@gmail.com>
2025-05-12 13:23:27 +0300

    git commit git push

--- client/src/main/java/ru/itmo/ki40lf/ClientMain.java
+++ client/src/main/java/ru/itmo/ki40lf/ClientMain.java
@@ -7,31 +7,34 @@ import java.util.Scanner;
 
 public class ClientMain {
     public static void main(String[] args) {
-        String host = "localhost";
-        int port = 12345;
-        try //(
-              //  ConnectionManager conn = new ConnectionManager(host, port)
-        //)
-        { ConnectionManager conn = new ConnectionManager(host, port);
-            conn.connect();
-            RequestReader reader = new RequestReader(new Scanner(System.in));
-            RequestWriter writer = new RequestWriter(conn.getOut());
-            ResponseReader responder = new ResponseReader(conn.getIn());
+//        String host = "localhost";
+//        int port = 12345;
+//        try //(
+//              //  ConnectionManager conn = new ConnectionManager(host, port)
+//        //)
+//        { ConnectionManager conn = new ConnectionManager(host, port);
+//            conn.connect();
+//            RequestReader reader = new RequestReader(new Scanner(System.in));
+//            RequestWriter writer = new RequestWriter(conn.getOut());
+//            ResponseReader responder = new ResponseReader(conn.getIn());
+//
+//            while (true) {
+//                Request request = reader.readRequest();
+//                writer.write(request);
+//
+//                Response response = responder.read();
+//                responder.print(response);
+//
+//                if ("exit".equalsIgnoreCase(request.getMessage())) {
+//                    break;
+//                }
+//            }
+//
+//        } catch (IOException | ClassNotFoundException e) {
+//            System.err.println("Ошибка клиента: " + e.getMessage());
+//        }
 
-            while (true) {
-                Request request = reader.readRequest();
-                writer.write(request);
-
-                Response response = responder.read();
-                responder.print(response);
-
-                if ("exit".equalsIgnoreCase(request.getMessage())) {
-                    break;
-                }
-            }
-
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Ошибка клиента: " + e.getMessage());
-        }
+        Client client = new Client();
+        client.run();
     }
 }
